fef232395bed54a94e959e95436ad408867c9779
Ramon <ramonads.42@gmail.com>
2025-04-17 18:17:17 -0300

    :tada: feat: Ajustando tamanho da fonte do footer

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -76,7 +76,7 @@ export default function Sidebar() {
           color: "var(--color-popover-foreground)",
         }}
       >
-        <img src="/adalove.jpeg" alt="Adalove" className="mx-auto w-16 h-16 rounded-full" />        <p className="text-sm mt-2">Equipe AdaLove®</p>
+        <img src="/adalove.jpeg" alt="Adalove" className="mx-auto w-16 h-16 rounded-full" />        <p className="text-base mt-2">Equipe AdaLove®</p>
         <p className="text-sm text-white-500">
           Acesse nosso{" "}
           <a
