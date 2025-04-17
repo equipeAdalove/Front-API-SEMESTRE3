33fff383cd638dda97350545d5ca2ca97d868ff4
Ramon <ramonads.42@gmail.com>
2025-04-17 16:17:59 -0300

    :tada: feat: Adicionando logo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -72,8 +72,7 @@ export default function Sidebar() {
           color: "var(--color-popover-foreground)",
         }}
       >
-        <img src="/logo.png" alt="Adalove" className="mx-auto w-12 h-12" />
-        <p className="text-sm mt-2">Equipe AdaLove®</p>
+        <img src="/adalove.jpeg" alt="Adalove" className="mx-auto w-12 h-12 rounded-full" />        <p className="text-sm mt-2">Equipe AdaLove®</p>
         <p className="text-xs text-white-500">
           Acesse nosso{" "}
           <a
