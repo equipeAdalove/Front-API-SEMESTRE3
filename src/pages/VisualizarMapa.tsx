d4ccead35ea93f7efef35724e566db4bd67fa88a
Maria Fernanda <160780186+Madhs31@users.noreply.github.com>
2025-04-18 13:28:04 -0300

    ✨ feat: Atualizando a página de Visualizar Mapa

--- src/pages/VisualizarMapa.tsx
+++ src/pages/VisualizarMapa.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import BrazilMap from "../components/map/MapaBrasil";
+import ThemeSwitcher from "@/components/theme-provider/ButtonThemeSwitcher";
 
 const NupPage = () => {
-  const [selectedState, setSelectedState] = useState(""); 
+  const [selectedState, setSelectedState] = useState("");
+  const navigate = useNavigate();
 
   const handleSelectState = (state: string) => {
     setSelectedState(state);
@@ -11,11 +14,33 @@ const NupPage = () => {
 
   return (
     <div>
-      <div>
-      <p className="text-sm text-gray-400">Pages / Visualizar Mapa</p>
-      <h1 className="text-4xl font-bold"> Visualizar Mapa</h1>
+      <div className="flex items-center justify-between">
+        <div>
+          <p className="text-sm text-gray-400">Pages / Visualizar Mapa</p>
+          <h1 className="text-4xl font-bold">Visualizar Mapa</h1>
+        </div>
+        <div className="flex items-center space-x-6 bg-[var(--color-card)] rounded-3xl px-6 py-3 shadow-lg">
+          <button
+            onClick={() => navigate("/importacao_map")}
+            className="text-[var(--color-primary)] hover:text-[var(--color-primary-light)] focus:outline-none transition-all duration-200"
+          >
+            Importação
+          </button>
+          <button>
+            
+          </button>
+          <button
+            onClick={() => navigate("/exportacao_map")}
+            className="text-[var(--color-primary)] hover:text-[var(--color-primary-light)] focus:outline-none transition-all duration-200"
+          >
+            Exportação
+          </button>
+          <ThemeSwitcher />
+        </div>
       </div>
+
       <BrazilMap onSelectState={handleSelectState} />
+
       {selectedState && (
         <p className="mt-4 text-md">
           Você clicou em: <strong>{selectedState}</strong>
@@ -26,4 +51,5 @@ const NupPage = () => {
 };
 
 export default NupPage;
+
 
