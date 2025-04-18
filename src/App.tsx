56508adb5b74dfdc2395e06feadfadeb12c60ea3
Maria Fernanda <160780186+Madhs31@users.noreply.github.com>
2025-04-18 12:07:42 -0300

    ✨ feat: Arrumando rota para a pagina VisualizarMapa.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { ThemeProvider } from "@/components/theme-provider/theme-provider";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DashboardInit from "./pages/DashboardInit"; // Página inicial do Dashboard
+import DashboardInit from "./pages/DashboardInit";
 import Sidebar from "./components/ui/sidebar";
 import BuscarNcm from "./pages/BuscarNcm";
-import ImportacaoPage from "./pages/ImportacaoPage";
 import VisualizarMapa from "./pages/VisualizarMapa";
+import ImportacaoPage from "./pages/ImportacaoPage";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
         <div className="flex h-screen">
-          {/* Sidebar agora estará dentro do ThemeProvider */}
           <Sidebar />
 
           <div className="flex-1 overflow-y-auto p-6">
@@ -22,9 +21,8 @@ function App() {
               <Route path="/estado/:nome" element={<DashboardInit />} />
               <Route path="/importacao" element={<ImportacaoPage />} />
               <Route path="/visualizar-mapa" element={<VisualizarMapa />} />
-
+              <Route path="/mapa" element={<VisualizarMapa />} /> {/* Nova rota aqui */}
               <Route path="/buscar-ncm" element={<BuscarNcm />} />
-              
             </Routes>
           </div>
         </div>
