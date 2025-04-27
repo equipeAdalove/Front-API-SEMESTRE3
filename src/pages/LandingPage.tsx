9fa5d034ecace35b4dda9ca61ca1977c08b0dc95
Ramon <ramonads.42@gmail.com>
2025-04-27 00:21:45 -0300

    :lipstick: Adiciona o FAQ e a parte de funcionalidades para a landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,10 +2,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
+import { useState } from 'react';
+
 import { Link } from "react-router-dom"; // Se estiver usando Next.js, troca para "next/link"
 import ThemeSwitcher from "@/components/theme-provider/ButtonThemeSwitcher"; // Botão de tema
 
+import { ChevronDown, LayoutDashboard, Map, Search} from 'lucide-react';
+
+
 export default function LandingPage() {
+  const [activeIndex, setActiveIndex] = useState(null);   // Estado para o FAQ
+    const toggleAnswer = (index) => {     // Função para abrir e fechar as perguntas do FAQ
+
+      setActiveIndex(activeIndex === index ? null : index); // Se a resposta já está aberta, fecha; senão, abre
+    };
   return (
     <main
       className="min-h-screen flex flex-col text-center"
@@ -53,17 +63,139 @@ export default function LandingPage() {
         Funcionalidades 
         (Cards com miniaturas: Dashboard Interativo, Mapa de Calor, Busca por NCM)
       */}
-      <section id="funcionalidades" className="w-full py-20 bg-purple-100">
+          <section id="funcionalidades" className="w-full py-20 bg-purple-100">
+          <div className="max-w-6xl mx-auto px-6">
+          <h2 className="text-6xl font-bold text-center mb-16 text-purple-600 dark:text-purple-400">
+            Funcionalidades do Produto
+          </h2>
 
-      </section>
+              {/* Card Dashboard Interativo */}
+              <div className="bg-white dark:bg-gray-700 rounded-xl shadow-md p-6 transition-all hover:shadow-lg">
+                <div className="flex items-center mb-4">
+                  <div className="bg-purple-100 dark:bg-purple-900 p-3 rounded-lg mr-4">
+                    <LayoutDashboard className="w-6 h-6 text-purple-600 dark:text-purple-400" />
+                  </div>
+                  <h3 className="text-xl font-bold text-gray-800 dark:text-gray-100">Dashboard Interativo</h3>
+                </div>
+                <p className="text-gray-600 dark:text-gray-300">
+                  Acesse gráficos e visualizações dinâmicas que facilitam a interpretação dos dados de importação e exportação dos estados brasileiros. Identifique tendências e tome decisões estratégicas com base em informações precisas.
+                </p>
+              </div>
+              
+            {/* Imagem do dashboard*/}
+            <div className="md:w-1/1">
+              <img 
+                src="src/components/images/funcionalidades/DashImg.png" 
+                alt="Dashboard Interface" 
+                className="w-full h-auto object-cover rounded-xl shadow-lg" 
+              />
+            </div>
+
+              
+              {/* Card Busca por NCM */}
+              <div className="bg-white dark:bg-gray-700 rounded-xl shadow-md p-6 transition-all hover:shadow-lg">
+                <div className="flex items-center mb-4">
+                  <div className="bg-green-100 dark:bg-green-900 p-3 rounded-lg mr-4">
+                    <Search className="w-6 h-6 text-green-600 dark:text-green-400" />
+                  </div>
+                  <h3 className="text-xl font-bold text-gray-800 dark:text-gray-100">Busca por NCM</h3>
+                </div>
+                <p className="text-gray-600 dark:text-gray-300">
+                  Busca de carga por meio do código NCM (Nomenclatura Comum do Mercosul).
+                </p>
+              </div>
+
+            {/* Imagem do dashboard*/}
+            <div className="md:w-1/1">
+              <img 
+                src="src/components/images/funcionalidades/NCMImg.png" 
+                alt="Dashboard Interface" 
+                className="w-full h-auto object-cover rounded-xl shadow-lg" 
+              />
+            </div>
+
+                          {/* Card Mapa de Calor */}
+                          <div className="bg-white dark:bg-gray-700 rounded-xl shadow-md p-6 ">
+                <div className="flex items-center mb-4">
+                  <div className="bg-blue-100 dark:bg-blue-900 p-3 rounded-lg mr-4">
+                    <Map className="w-6 h-6 text-blue-600 dark:text-blue-400" />
+                  </div>
+                  <h3 className="text-xl font-bold text-gray-800 dark:text-gray-100">Mapa de Calor</h3>
+                </div>
+                <p className="text-gray-600 dark:text-gray-300">
+                  Visualize o desempenho do comércio exterior de cada estado brasileiro em um mapa de calor dinâmico, onde as cores representam variações nos volumes de importação e exportação.
+                </p>
+              </div>
+          <div className="md:w-1/1">
+              <img 
+                src="src/components/images/funcionalidades/MapImg.png" 
+                alt="Dashboard Interface" 
+                className="w-full h-auto object-cover rounded-xl shadow-lg" 
+              />
+            </div>
+        </div>
+        </section>
 
       {/* 
         FAQ 
         (Accordion simples de perguntas)
       */}
-      <section id="faq" className="w-full py-20">
+    <section id="faq" className="w-full py-20">
+      <div className="max-w-5xl mx-auto px-6">
+          <h2 className="text-6xl font-bold text-center mb-12 text-purple-600">
+            Dúvidas Frequentes (FAQ)
+          </h2>
+          
+          <div className="space-y-4">
+            {/* Pergunta 1 */}
+  <div className="border border-gray-300 rounded-lg overflow-hidden">
+    <div className="flex justify-between p-4 cursor-pointer bg-white" onClick={() => toggleAnswer(0)}>
+      <h3 className="text-lg font-medium text-gray-700">Quais dados estão disponíveis na plataforma?</h3>
+      <button className="text-gray-500">
+      <ChevronDown className="h-6 w-6" />
+      </button>
+    </div>
+    <div className={`p-4 bg-white border-t border-gray-200 ${activeIndex === 0 ? 'block' : 'hidden'}`}>
+      <p className="text-gray-600">
+        A plataforma AdaTrade disponibiliza dados de comércio exterior do Brasil, incluindo estatísticas de balança comercial de todos os estados brasileiros, dados históricos de 2014 a 2024, classificação por meio de transporte, pesquisa por código NCM, e tendências de crescimento ou declínio.
+      </p>
+    </div>
+  </div>
+  
+  {/* Pergunta 2 */}
+  <div className="border border-gray-300 rounded-lg overflow-hidden">
+    <div className="flex justify-between p-4 cursor-pointer bg-white" onClick={() => toggleAnswer(1)}>
+      <h3 className="text-lg font-medium text-gray-700">Posso acessar o AdaTrade gratuitamente?</h3>
+      <button className="text-gray-500">
+      <ChevronDown className="h-6 w-6" />
 
-      </section>
+      </button>
+    </div>
+    <div className={`p-4 bg-white border-t border-gray-200 ${activeIndex === 1 ? 'block' : 'hidden'}`}>
+      <p className="text-gray-600">
+        Sim! O AdaTrade é uma plataforma gratuita desenvolvida como projeto acadêmico pela equipe AdaLove da FATEC - Prof. Jessen Vidal. Nosso objetivo é democratizar o acesso aos dados de comércio exterior brasileiro.
+      </p>
+    </div>
+  </div>
+  
+  {/* Pergunta 3 */}
+  <div className="border border-gray-300 rounded-lg overflow-hidden">
+    <div className="flex justify-between p-4 cursor-pointer bg-white" onClick={() => toggleAnswer(2)}>
+      <h3 className="text-lg font-medium text-gray-700">Como posso utilizar os dados do AdaTrade para meu negócio?</h3>
+      <button className="text-gray-500">
+      <ChevronDown className="h-6 w-6" />
+
+      </button>
+    </div>
+    <div className={`p-4 bg-white border-t border-gray-200 ${activeIndex === 2 ? 'block' : 'hidden'}`}>
+      <p className="text-gray-600">
+      Os dados do AdaTrade permitem identificar tendências de mercado, comparar o desempenho comercial entre estados e realizar análises detalhadas por meio de visualizações interativas e exportação para ferramentas como PowerBI. Pesquise por NCM para acessar informações específicas sobre produtos e utilize nosso mapa interativo para compreender fluxos comerciais estratégicos.
+      </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
 
       {/* 
         Sobre nós 
