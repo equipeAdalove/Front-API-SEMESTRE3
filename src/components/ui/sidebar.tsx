dbcbe48ec0208376a1ac4b883d85cdba5ab00d9e
Raphaela Monteiro <bmraphaela@gmail.com>
2025-04-29 17:24:50 -0300

    :lipstick: feat: ajustes na sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -5,70 +5,70 @@ import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <aside
-      className="h-screen w-64 shadow-md flex flex-col p-4 bg-[var(--color-sidebar)] text-[var(--color-sidebar-foreground)]"
+    <aside className="h-screen w-64 shadow-md flex flex-col p-4 bg-[var(--color-sidebar)] text-[var(--color-sidebar-foreground)]"
     >
       {/* Header */}
-      <div className="w-full border-b border-gray-300 pb-4">
+      <div className="w-full pb-4">
         <Link to="/" className="block mt-5 text-4xl font-bold text-center hover:underline">
           <span className="text-[var(--ada)]">Ada</span>
           <span className="text-[var(--trade)]">Trade</span>
         </Link>
+        {/* Navigation - Centralizado Verticalmente */}
+        <nav className="flex-1 flex flex-col justify-center mt-10">
+          <ul className="space-y-4">
+            <li>
+              <Link to="/dashboard">
+                <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
+                  <Home style={{ transform: "scale(1.3)" }} />
+                  <span className="text-sm:text-base">Dashboard</span>
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/buscar-ncm">
+                <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
+                  <BarChart style={{ transform: "scale(1.3)" }} />
+                  <span className="text-sm:text-base">Buscar NCM</span>
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/mapa">
+                <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
+                  <Map style={{ transform: "scale(1.3)" }} />
+                  <span className="text-sm:text-base">Visualizar Mapa</span>
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/exportar">
+                <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
+                  <FileBarChart2 style={{ transform: "scale(1.3)" }} />
+                  <span className="text-sm:text-base">Exportar - Power BI</span>
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/tendencias">
+                <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
+                  <TrendingUp style={{ transform: "scale(1.3)" }} />
+                  <span className="text-sm:text-base">Tendências</span>
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/comparacoes">
+                <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
+                  <Sliders style={{ transform: "scale(1.3)" }} />
+                  <span className="text-sm:text-base">Comparações</span>
+                </Button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      {/* Navigation - Centralizado Verticalmente */}
-      <nav className="flex-1 flex flex-col justify-center mt-2">
-        <ul className="space-y-4">
-          <li>
-            <Link to="/dashboard">
-              <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
-                <Home style={{ transform: "scale(1.3)" }} />
-                <span className="text-sm:text-base">Dashboard</span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/buscar-ncm">
-              <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
-                <BarChart style={{ transform: "scale(1.3)" }} />
-                <span className="text-sm:text-base">Buscar NCM</span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/mapa">
-              <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
-                <Map style={{ transform: "scale(1.3)" }} />
-                <span className="text-sm:text-base">Visualizar Mapa</span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/exportar">
-              <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
-                <FileBarChart2 style={{ transform: "scale(1.3)" }} />
-                <span className="text-sm:text-base">Exportar - Power BI</span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/tendencias">
-              <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
-                <TrendingUp style={{ transform: "scale(1.3)" }} />
-                <span className="text-sm:text-base">Tendências</span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/comparacoes">
-              <Button variant="ghost" className="w-full flex items-center gap-3 justify-start">
-                <Sliders style={{ transform: "scale(1.3)" }} />
-                <span className="text-sm:text-base">Comparações</span>
-              </Button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+
 
       {/* Footer */}
       <Card className="p-4 text-center mt-5"
