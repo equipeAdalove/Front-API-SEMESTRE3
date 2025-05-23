db790e3858997596bf83057bb749cb01979ff91d
Madhs31 <fernandinha.mf30@gmail.com>
2025-05-23 09:48:21 -0300

    💄 feat: Atualizando páginas

--- src/pages/VisualizarMapa.tsx
+++ src/pages/VisualizarMapa.tsx
@@ -6,9 +6,9 @@ function NupPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen p-8">
       {/* Topo com título e barra de busca */}
-      <div className="flex justify-between items-center mt-10 mb-10 flex-wrap gap-4 px-10">
+      <div className="flex justify-between items-center mt-4 mb-10 flex-wrap gap-4 px-3">
         <div className="-mt-2">
           <p className="text-sm text-[var(--muted-foreground)]">
             Pages / Visualizar Mapas
@@ -17,19 +17,21 @@ function NupPage() {
         </div>
 
         <div className="flex items-center space-x-6 bg-[var(--color-card)] rounded-3xl px-6 py-3 shadow-lg">
-          <button className="text-[var(--color-primary)] font-semibold">
-            Importação
-          </button>
-          <button
-            onClick={() => navigate("/exportacao_mapa")}
-            className="text-[var(--color-primary)] hover:text-[var(--color-primary-light)] focus:outline-none transition-all duration-200"
-          >
-            Exportação
-          </button>
-          <ThemeSwitcher />
-        </div>
+            <button
+              onClick={() => navigate("/importacao")}
+              className="text-[var(--color-primary)] hover:text-[var(--color-primary-light)]"
+            >
+              Importação
+            </button>
+            <button
+              onClick={() => navigate("/exportacao")}
+              className="text-[var(--color-primary)] hover:text-[var(--color-primary-light)]"
+            >
+              Exportação
+            </button>
+            <ThemeSwitcher />
+          </div>
       </div>
-
       {/* HeatMap renderizado */}
       <HeatMap />
     </div>
